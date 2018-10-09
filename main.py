8edd8a55ed2b74be5ab9a6ee48adebefbc180e1b
Beants <38414842@qq.com>
2018-10-09 11:43:04 +0800

    更新图表

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 # @Software: PyCharm
 
 from flask import Flask, render_template, request
-from pyecharts import Overlap
+from pyecharts import Grid, Overlap, Line
 
 import config
 import mongo
@@ -33,7 +33,7 @@ def dataManagerStudent():
     if stuID == None:
         return render_template('dataManagerStudent.html', data=None)
     else:
-        from pyecharts import Bar, Line
+        from pyecharts import Bar
         from pyecharts_javascripthon.api import TRANSLATOR
 
         REMOTE_HOST = "https://pyecharts.github.io/assets/js"
@@ -41,6 +41,10 @@ def dataManagerStudent():
         data, len_data = mongo.get_data_by_condition(request.args.get('stuID'))
         data_day_out = {}
         data_day_in = {}
+        data_shop = []
+        data_shop_money = {}  # 这个月花费的金额
+        data_shop_num = {}  # 这个月消费的次数
+        data_shop_money_avg = {}  # 每次消费平均金额
         import time
         fun = lambda year, month: list(
             range(1, 1 + time.localtime(time.mktime((year, month + 1, 1, 0, 0, 0, 0, 0, 0)) - 86400).tm_mday))
@@ -55,32 +59,41 @@ def dataManagerStudent():
             data_day_in[str(item)] = 0
             data_day_out[str(item)] = 0
 
+
         for item in data:
             time = str(int(str(item[4]).split(' ')[0].split('-')[2]))
             # print(time)
             money = -float(item[3])
+            shop = str(item[2])
+            if shop == '407010002':
+                pass
+            else:
+                shop = shop.replace(str(item[2])[:3], str(item[2])[:3] + '\n')
+                if shop not in data_shop:
+                    data_shop.append(shop)
+                    data_shop_money[shop] = money
+                    data_shop_money[shop] = float(format(data_shop_money[shop], '.2f'))
+                    data_shop_num[shop] = 1
+                else:
+                    data_shop_money[shop] += money
+                    data_shop_money[shop] = float(format(data_shop_money[shop], '.2f'))
+                    data_shop_num[shop] += 1
             if money > 0:
                 data_day_out[time] += money
                 data_day_out[time] = float(format(data_day_out[time], '.2f'))
             else:
                 data_day_in[time] += money
                 data_day_in[time] = float(format(data_day_in[time], '.2f'))
-        bar = Bar("", "")
 
-        bar.add('消费金额', days, list(data_day_out.values()))
-        bar.add('充值金额', days, list(data_day_in.values()), bar_category_gap=0.05)
-        line = Line()
-
-        line.add("消费均值", days, [format(sum(list(data_day_out.values())) / len(days), '.2f')] * list(
-            data_day_out.values()).__len__())
-        # line.add("消费均值", days,
-        #          sum(list(data_day_out.values())) / (list(data_day_out.values()).__len__()))  # print(len_data)
-
-        overlap = Overlap()
-        overlap.add(bar)
-        overlap.add(line)
-        javascript_snippet = TRANSLATOR.translate(overlap.options)
-
+        # data_shop = data_shop.sort()
+        # 计算每次在店消费的平均值
+        for i in data_shop:
+            data_shop_money_avg[i] = float(format(data_shop_money[i] / data_shop_num[i], '.2f'))
+        # data_shop.pop(data_shop.index('407010002'))
+        # data_shop_money.pop('407010002')
+        # data_shop_num.pop('407010002')
+        # data_shop_money_avg.pop('407010002')
+        # 拼接返回值
         no_out_days_num = 0
         no_out_days = []
         for i in data_day_out.keys():
@@ -95,8 +108,8 @@ def dataManagerStudent():
                 avg             平均值
                 in              充值金额
                 not_eat_days    没有消费的天数
-                
-                
+
+
                 '''
         res_analyse = {
             'max': format(max(list(data_day_out.values())), '.2f'),
@@ -107,16 +120,43 @@ def dataManagerStudent():
             'no_out_days_num': no_out_days_num,
             'no_out_days': no_out_days.__str__().replace('[', '').replace(']', '').replace("'", ''),
         }
+
+        ##########图
+
+        bar = Bar(height=600)  # 柱形图
+
+        bar.add('消费金额', days, list(data_day_out.values()), mark_line=["average"], mark_point=["max", "min"],
+                )
+        bar.add('充值金额', days, list(data_day_in.values()), bar_category_gap=0.05, mark_line=["average"],
+                mark_point=["max", "min"])
+
+        overlap = Overlap()
+        bar2 = Bar("消费地点分析", title_top="50%", )
+        bar2.add('消费金额', data_shop, list(data_shop_money.values()), mark_point=["max", "min"], legend_top="50%")
+        bar2.add('消费次数', data_shop, list(data_shop_num.values()), mark_point=["max", "min"], legend_top="50%")
+
+        line = Line()
+        line.add('消费均值', data_shop, list(data_shop_money_avg.values()), legend_top="50%", mark_point=["max", "min"])
+
+        overlap.add(bar2)
+        overlap.add(line)
+        grid = Grid()
+        grid.add(bar, grid_bottom="60%")
+        grid.add(overlap, grid_top="60%")
+
+        javascript_snippet = TRANSLATOR.translate(grid.options)
+
+
         return render_template('dataManagerStudent.html',
                                data=res_analyse,
-                               chart_id=overlap.chart_id,
+                               chart_id=grid.chart_id,
                                host=REMOTE_HOST,
-                               renderer=overlap.renderer,
+                               renderer=grid.renderer,
                                my_width="100%",
-                               my_height=600,
+                               my_height=1200,
                                custom_function=javascript_snippet.function_snippet,
                                options=javascript_snippet.option_snippet,
-                               script_list=overlap.get_js_dependencies())
+                               script_list=grid.get_js_dependencies())
 
 
 @app.route('/dataManager/')
